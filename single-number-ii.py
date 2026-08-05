0f8c48a5dae1a14461fa1d72b6f919c79a03398e
motyzk <motyzk@gmail.com>
2026-08-05 17:31:08 +0300

    fixed bug - num of bits 32 vs 15

--- single-number-ii.py
+++ single-number-ii.py
@@ -4,7 +4,7 @@ class Solution:
     def singleNumber(self, nums):
         pos_result = 0
         neg_result = 0
-        for i in range(15):
+        for i in range(32):
             pos_curr = 0
             neg_curr = 0
             for n in nums:
@@ -12,13 +12,14 @@ class Solution:
                     pos_curr += 1
                 if n < 0 and (-n & (1 << i)):
                     neg_curr += 1
-            pos_result += (pos_curr % 2) * (2 ** i)
-            neg_result += (neg_curr % 2) * (2 ** i)
+            pos_result += (pos_curr % 3) * (2 ** i)
+            neg_result += (neg_curr % 3) * (2 ** i)
         return pos_result - neg_result
 
 s = Solution()
 assert s.singleNumber([2, 2, 2, 1]) == 1
 assert s.singleNumber([2, 2, 4, 4, 2, 4, 1]) == 1
+assert s.singleNumber([2, 2, 4, 4, 2, 4, -1]) == -1
 assert s.singleNumber([2, 4, 2, 4, 2, 4, 1]) == 1
 assert s.singleNumber([2, 4, 2, 4, 2, 1, 4]) == 1
 assert s.singleNumber([1]) == 1
